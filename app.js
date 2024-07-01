b231d6d57b1bc4bc4926510e43372250c0d9f800
Delta-Student <aparnaghora2002@gmail.com>
2024-07-01 16:35:23 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ if(process.env.NODE_ENV != "production") {
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 const path = require("path");
